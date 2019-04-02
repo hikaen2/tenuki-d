dd1a848ce9eade35572faa889fd13e528d969e54
hikaen2 <hikaen2>
2019-04-02 17:16:11 +0900

    wip

--- source/eval_twig.d
+++ source/eval_twig.d
@@ -16,9 +16,8 @@ static this()
 {
     {
         File f = File("KK_synthesized.bin", "r");
-        scope (exit) {
-            f.close();
-        }
+        scope (exit) f.close();
+        assert(f.size == 52488); // 81 x 81 x 8
         for (int i = 0; i < 81; i++) {
             for (int j = 0; j < 81; j++) {
                 int[2] buf;
@@ -29,9 +28,8 @@ static this()
 
     {
         File f = File("KKP_synthesized.bin", "r");
-        scope (exit) {
-            f.close();
-        }
+        scope (exit) f.close();
+        assert(f.size == 81251424); // 81 x 81 x 1548 x 8
         for (int i = 0; i < 81; i++) {
             for (int j = 0; j < 81; j++) {
                 for (int k = 0; k < 1548; k++) {
@@ -44,9 +42,8 @@ static this()
 
     {
         File f = File("KPP_synthesized.bin", "r");
-        scope (exit) {
-            f.close();
-        }
+        scope (exit) f.close();
+        assert(f.size == 776402496); // 81 x 1548 x 1548 x 4
         for (int i = 0; i < 81; i++) {
             for (int j = 0; j < 1548; j++) {
                 for (int k = 0; k < 1548; k++) {
@@ -76,55 +73,70 @@ short staticValue(const ref Position p)
     int material = 0;
     int bk = 0;
     int wk = 0;
-    for (int i = SQ11; i <= SQ99; i++) {
-        material += P_VALUE[p.board[i].i];
-        bk = (p.board[i] == Square.B_KING) ? i : bk;
-        wk = (p.board[i] == Square.W_KING) ? i : wk;
-    }
+    int[38] list;
+    int[38] list_inv;
+    int length = 0;
+
     // 持ち駒
     for (int t = Type.PAWN; t <= Type.ROOK; t++) {
         material += (p.piecesInHand[Color.BLACK][t] - p.piecesInHand[Color.WHITE][t]) * P_VALUE[t];
+        for (int i = 0; i < p.piecesInHand[Color.BLACK][t]; i++) {
+            list    [length] = OFFSET_HAND[Color.BLACK][t] + i;
+            list_inv[length] = OFFSET_HAND[Color.WHITE][t] + i;
+        }
+        for (int i = 0; i < p.piecesInHand[Color.WHITE][t]; i++) {
+            list    [length] = OFFSET_HAND[Color.BLACK][t] + i;
+            list_inv[length] = OFFSET_HAND[Color.WHITE][t] + i;
+        }
+        length++;
     }
-
-    // リストを作る
-    short[40] list_fb;
-    short[40] list_fw;
-    int length = 0;
     for (int i = SQ11; i <= SQ99; i++) {
-        if (p.board[i] != Square.EMPTY && p.board[i].type != Type.KING) {
-            list_fb[length] = cast(short)(PP_OFFSET[p.board[i].i] + i);
-            list_fw[length] = cast(short)(PP_OFFSET[p.board[i].i] + i);
+        Square sq = p.board[i];
+        material += P_VALUE[sq.i];
+        bk = (sq == Square.B_KING) ? i : bk;
+        wk = (sq == Square.W_KING) ? i : wk;
+        if (sq != Square.EMPTY && sq.type != Type.KING) {
+            list    [length] = OFFSET_PP[sq.i] + i;
+            list_inv[length] = OFFSET_PP[sq.inv.i] + (SQ99 - i);
             length++;
         }
     }
+    assert(length == 38);
+
+    int sum0a, sum0b; // 先手玉から見たKPP,  先手玉から見たKPPの先手加算
+    int sum1a, sum1b; // 後手玉から見たKPP,  後手玉から見たKPPの先手加算
+    int sum2a, sum2b; // KK + KKP,  KKの先手加算 + KKPの先手加算
 
     // KK
-    int sum2_0 = KK[bk][wk][0];
-    int sum2_1 = KK[bk][wk][1];
-
-    for (i = 0; i < length ; i++) {
-        int k0 = list_fb[i];
-        int k1 = list_fw[i];
+    sum2a = KK[bk][wk][0];
+    sum2b = KK[bk][wk][1];
+    for (int i = 0; i < 38; i++) {
+        const int k     = list[i];
+        const int k_inv = list_inv[i];
 
         // KKP
-        sum2_0 += KKP[bk][wk][k0][0];
-        sum2_1 += KKP[bk][wk][k0][1];
+        sum2a += KKP[bk][wk][k][0];
+        sum2b += KKP[bk][wk][k][1];
 
-        for (j = 0; j < i; j++) {
-            int l0 = list_fb[j];
-            int l1 = list_fw[j];
+        for (int j = 0; j < i; j++) {
+            const int l     = list[j];
+            const int l_inv = list_inv[j];
 
             // KPP
-            sum0_0 += KPP[bk][k0][l0][0];
-            sum0_1 += KPP[bk][k0][l0][1];
-            sum1_0 += KPP[inv(wk)][k1][l1][0];
-            sum1_1 += KPP[inv(wk)][k1][l1][1];
+            sum0a += KPP[bk][k][l][0];
+            sum0b += KPP[bk][k][l][1];
+            sum1a += KPP[SQ99 - wk][k_inv][l_inv][0];
+            sum1b += KPP[SQ99 - wk][k_inv][l_inv][1];
         }
     }
 
-    sum /= FV_SCALE;
-    int value = material + sum;
-    return cast(short)(p.sideToMove == Color.BLACK ? value : -value);
+    // 手番に依存しない評価値合計
+    const int scoreBoard = sum0a - sum1a + sum2a + (material * FV_SCALE); // 先手玉から見たKKP - 後手玉から見たKPP + KK + KKP + 駒割
+
+    // 手番に依存する評価値合計
+    const int scoreTurn  = sum0b + sum1b + sum2b; // 先手玉から見たKKPの先手加算 + 後手玉から見たKPPの先手加算 + KKの先手加算 + KKPの先手加算
+
+    return cast(short)(((p.sideToMove == Color.BLACK ? scoreBoard : -scoreBoard) + scoreTurn) / FV_SCALE);
 }
 */
 
@@ -156,7 +168,7 @@ private immutable short[][] OFFSET_HAND = [
  * key: Square
  * see https://github.com/HiraokaTakuya/apery/blob/32216277e51c3b008e3c8eea6954f1bb3c416b57/src/evaluate.hpp#L36
  */
-private immutable short[] PP_OFFSET = [
+private immutable short[] OFFSET_PP = [
   // 歩,  香,  桂,  銀,  金,  角,   飛, 王,  と, 成香, 成桂, 成銀,   馬,   龍,
      90, 252, 414, 576, 738, 900, 1224,  0, 738,  738,  738,  738, 1062, 1386,
     171, 333, 495, 657, 819, 981, 1305,  0, 819,  819,  819,  819, 1143, 1467,

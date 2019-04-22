61f3030a77c986e2362e2468d6b428e42cacafbf
hikaen2 <hikaen2>
2019-04-23 01:47:51 +0900

    wip

--- source/tt.d
+++ source/tt.d
@@ -4,9 +4,14 @@ import std.stdio;
 import core.atomic;
 import types;
 import text;
+import core.atomic;
 
-private immutable MASK = 0xffffff; // 1024 * 1024 * 16 - 1
+private immutable MASK = 0x7ffffff; // 1024 * 1024 * 16 - 1
 __gshared private TTEntry[MASK + 1] TT;
+shared long stat_nothing = 0;
+shared long stat_misshit = 0;
+shared long stat_hit = 0;
+shared long stat_stored = 0;
 
 struct TTEntry
 {
@@ -16,18 +21,39 @@ struct TTEntry
 
 Move probe(ulong key)
 {
-    TTEntry e = TT[key & MASK];
-
-    if (e.key64 == key)
+    for (int i = 0; i < 5; i++)
     {
-        return cast(Move)(e.move16);
+        TTEntry e = TT[((key & MASK) + i * 2) % (MASK + 1)];
+        if (e.key64 == 0)
+        {
+            atomicOp!"+="(stat_nothing, 1);
+            return cast(Move)(0);
+        }
+        if (e.key64 == key)
+        {
+            atomicOp!"+="(stat_hit, 1);
+            return cast(Move)(e.move16);
+        }
+        //atomicOp!"+="(stat_misshit, 1);
+        //return cast(Move)(0);
     }
+    atomicOp!"+="(stat_misshit, 1);
     return cast(Move)(0);
 }
 
 void store(ulong key, Move m)
 {
-    TT[key & MASK] = TTEntry(key, m.i);
+    for (int i = 0; i < 5; i++)
+    {
+        const long address = ((key & MASK) + i * 2) % (MASK + 1);
+        if (TT[address].key64 == 0 || TT[address].key64 == key)
+        {
+            TT[address] = TTEntry(key, m.i);
+            atomicOp!"+="(stat_stored, 1);
+            return;
+        }
+        //return;
+    }
 }
 
 long hashfull()
@@ -37,5 +63,6 @@ long hashfull()
     {
         cnt += e.move16 == 0 ? 0 : 1;
     }
-    return cnt * 1000 / (MASK + 1);
+    return cnt;
+    //return cnt * 1000 / (MASK + 1);
 }
